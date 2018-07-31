80f8a714e3447db332e574dbc362c6a45ef10a6a
Adrien Aucher <aucher.adrien@gmail.com>
2018-07-31 15:11:13 +0100

    Use read_{bool,int,double} in config_read_preset.

--- config.c
+++ config.c
@@ -9,16 +9,32 @@
 #include "types.h"
 #include "panic.h"
 
+static void read_bool(toml_table_t* conf, const char* key, bool* dest, bool fallback) {
+    const char* val = NULL;
+    if (!((val = toml_raw_in(conf, key))
+            && toml_rtob(val, (int*)dest) == 0)) {
+        *dest = fallback;
+    }
+}
+static void read_int(toml_table_t* conf, const char* key, int* dest, int fallback) {
+    const char* val = NULL;
+    if (!((val = toml_raw_in(conf, key))
+            && toml_rtoi(val, (int64_t*)dest) == 0)) {
+        *dest = fallback;
+    }
+}
+static void read_double(toml_table_t* conf, const char* key, double* dest, double fallback) {
+    const char* val = NULL;
+    if (!((val = toml_raw_in(conf, key))
+            && toml_rtod(val, dest) == 0)) {
+        *dest = fallback;
+    }
+}
+
 static struct fractal_info* config_read_preset(toml_table_t* preset) {
     struct fractal_info* fi = calloc(1, sizeof(struct fractal_info));
     /* Read preset. */
     const char* val = NULL;
-    /* key: name */
-    if ((val = toml_raw_in(preset, "name"))) {
-        //...
-    } else {
-        //...
-    }
     /* key: generator */
     if ((val = toml_raw_in(preset, "generator"))) {
         char* gen = NULL;
@@ -34,74 +50,32 @@ static struct fractal_info* config_read_preset(toml_table_t* preset) {
         fi->generator = GEN_MANDELBROT;
     }
     /* key: dynamic */
-    if (!((val = toml_raw_in(preset, "dynamic"))
-            && toml_rtob(val, (int*)&fi->dynamic) == 0)) {
-        fi->dynamic = false;
-    }
+    read_bool(preset, "dynamic", &(fi->dynamic), false);
     /* key: speed */
-    if (!((val = toml_raw_in(preset, "speed"))
-            && toml_rtod(val, &(fi->speed)) == 0)) {
-        fi->speed = 0.0;
-    }
+    read_double(preset, "speed", &(fi->speed), 0.0);
     /* table: center */
     toml_table_t* center;
     if ((center = toml_table_in(preset, "center"))) {
         /* key: center.x */
-        if (!((val = toml_raw_in(center, "x"))
-                && toml_rtod(val, &(fi->cx)) == 0)) {
-            fi->cx = 0.0;
-        }
+        read_double(center, "x", &(fi->cx), 0.0);
         /* key: center.y */
-        if (!((val = toml_raw_in(center, "y"))
-                && toml_rtod(val, &(fi->cy)) == 0)) {
-            fi->cy = 0.0;
-        }
+        read_double(center, "y", &(fi->cy), 0.0);
     }
     /* key: dpp */
-    if (!((val = toml_raw_in(preset, "dpp"))
-            && toml_rtod(val, &(fi->dpp)) == 0)) {
-        fi->dpp = 0.0;
-    }
+    read_double(preset, "dpp", &(fi->dpp), 0.0);
     /* key: max_iter */
-    if (!((val = toml_raw_in(preset, "max_iter"))
-            && toml_rtoi(val, (int64_t*)&(fi->max_iter)) == 0)) {
-        fi->max_iter = 50;
-    }
+    read_int(preset, "max_iter", &(fi->max_iter), 0);
     /* table: julia */
     toml_table_t* julia;
     if ((julia = toml_table_in(preset, "julia"))) {
         /* key: julia.x */
-        if (!((val = toml_raw_in(julia, "x"))
-                && toml_rtod(val, &(fi->jx)) == 0)) {
-            fi->jx = 0.0;
-        }
+        read_double(julia, "x", &(fi->jx), 0.0);
         /* key: julia.y */
-        if (!((val = toml_raw_in(julia, "y"))
-                && toml_rtod(val, &(fi->jy)) == 0)) {
-            fi->jy = 0.0;
-        }
+        read_double(julia, "y", &(fi->jy), 0.0);
     }
     /* key: n */
-    if (!((val = toml_raw_in(preset, "n"))
-            && toml_rtoi(val, (int64_t*)&(fi->n)) == 0)) {
-        fi->n = 0;
-    }
+    read_int(preset, "n", &(fi->n), 0);
     return fi;
-}
-
-static void read_int(toml_table_t* conf, const char* key, int* dest, int fallback) {
-    const char* val = NULL;
-    if (!((val = toml_raw_in(conf, key))
-            && toml_rtoi(val, (int64_t*)dest) == 0)) {
-        *dest = fallback;
-    }
-}
-static void read_double(toml_table_t* conf, const char* key, double* dest, double fallback) {
-    const char* val = NULL;
-    if (!((val = toml_raw_in(conf, key))
-            && toml_rtod(val, dest) == 0)) {
-        *dest = fallback;
-    }
 }
 
 static void config_read_base(toml_table_t* conf, struct config* cfg) {
