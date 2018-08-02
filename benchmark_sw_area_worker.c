8ee78bafcb4338b1a763342b38608f1f15fb2100
Adrien Aucher <aucher.adrien@gmail.com>
2018-08-02 18:53:15 +0100

    Add benchmarks for line_worker & area_worker.

---
+++ benchmark_sw_area_worker.c
@@ -0,0 +1,41 @@
+#include "renderer_software.c"
+
+#ifndef WIDTH
+#define WIDTH 800
+#endif
+
+#ifndef HEIGHT
+#define HEIGHT 600
+#endif
+
+#ifndef TIMES
+#define TIMES 1000
+#endif
+
+#ifndef WORKER
+#define WORKER rdr_sw_area_worker
+#endif
+
+int main(void)
+{
+    /* Init */
+    SDL_Surface* buffer;
+    buffer = SDL_CreateRGBSurface(0, WIDTH, HEIGHT, 32, 0, 0, 0, 0);
+    struct fractal_info fi = {
+        .generator = GEN_MANDELBROT,
+        .max_iter  = 50,
+        .cx        = -0.7,
+        .cy        = 0.0,
+        .dpp       = 0.0035,
+    };
+
+    /* Benchmark */
+    for (int i = 0; i < TIMES; i++) {
+        rdr_sw_update(buffer, fi, 0.0, WORKER);
+    }
+
+    /* Cleanup */
+    SDL_FreeSurface(buffer);
+
+    return EXIT_SUCCESS;
+}
